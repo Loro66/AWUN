cea4d51cf808c6fd591d957e41406409b55d29d8
Loro66 <142999579+Loro66@users.noreply.github.com>
2026-07-16 20:09:42 +0300

    Add repeat modes and Windows icon

--- backend/core/config.py
+++ backend/core/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     )
 
     app_name: str = "AWUN"
-    app_version: str = "1.6.1"
+    app_version: str = "1.6.2"
     debug: bool = False
     api_prefix: str = "/api/v1"
 
