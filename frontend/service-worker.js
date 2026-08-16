9c2e4c1c16ca2a2c37ff2207eac300f4d6f29540
Loro66 <142999579+Loro66@users.noreply.github.com>
2026-08-16 20:29:26 +0300

    Arrange desktop player as listening room

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='awun-shell-1.8.4';
-const SHELL=['/','/static/styles.css?v=20260816.2','/static/forest.css?v=20260816.2','/static/desktop-bridge.js?v=20260816.2','/static/i18n.js?v=20260816.2','/static/app.js?v=20260816.2','/static/flow.js?v=20260816.2','/static/brand/black-forest-michiel-annaert.webp','/static/brand/awun-icon-black.png','/static/brand/awun-icon-white.png'];
+const CACHE='awun-shell-1.8.5';
+const SHELL=['/','/static/styles.css?v=20260816.3','/static/forest.css?v=20260816.3','/static/desktop-bridge.js?v=20260816.3','/static/i18n.js?v=20260816.3','/static/app.js?v=20260816.3','/static/flow.js?v=20260816.3','/static/brand/black-forest-michiel-annaert.webp','/static/brand/awun-icon-black.png','/static/brand/awun-icon-white.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
